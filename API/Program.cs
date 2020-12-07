648d50c387e7ee3976bd673594ed10901075475f
transtrike <transtrike@gmail.com>
2020-12-07 22:39:49 +0200

    API folder fixed

---
+++ API/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace API
+{
+	public class Program
+    {
+        private const int HTTPS_PORT = 5020;
+
+		public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.ConfigureKestrel(opt => opt.ListenLocalhost(HTTPS_PORT));
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
