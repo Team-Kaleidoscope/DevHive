ca9d36e8625d916ff2a38b60cbca084e9318deb6
Syndamia <kamen.d.mladenov@protonmail.com>
2020-12-08 17:28:11 +0200

    Replaced identation spaces with tabs in User class

--- Models/Classes/User.cs
+++ Models/Classes/User.cs
@@ -6,76 +6,76 @@ using System.Collections.Generic;
 
 namespace Models.Classes
 {
-    [Table("Users")]
+	[Table("Users")]
 	public class User<T> : IdentityUser<int>
 	{
-        private string firstName;
-        private string lastName;
-        private string profilePicture;
-        
-        [Required]
-        [Range(3, 50)]
-        [Display(Name = "Username")]
-        public override string UserName {
-            get => base.UserName;
-            set {
-                ValidateString("Username", 3, 50, value, true);
-                base.UserName = value;
-            }
-        }
-        
-        [Required]
-        [Range(3, 30)]
-        public string FirstName {
-            get => this.firstName;
-            set {
-                ValidateString("FirstName", 3, 30, value, false);
-                this.firstName = value;
-            }
-        }
+		private string firstName;
+		private string lastName;
+		private string profilePicture;
 
-        [Required]
-        [Range(3, 30)]
-        public string LastName { 
-            get => this.lastName;
-            set {
-                ValidateString("LastName", 3, 30, value, false);
-                this.lastName = value;
-            }
-        }
+		[Required]
+		[Range(3, 50)]
+		[Display(Name = "Username")]
+		public override string UserName {
+			get => base.UserName;
+			set {
+				ValidateString("Username", 3, 50, value, true);
+				base.UserName = value;
+			}
+		}
+		
+		[Required]
+		[Range(3, 30)]
+		public string FirstName {
+			get => this.firstName;
+			set {
+				ValidateString("FirstName", 3, 30, value, false);
+				this.firstName = value;
+			}
+		}
+		
+		[Required]
+		[Range(3, 30)]
+		public string LastName { 
+			get => this.lastName;
+			set {
+				ValidateString("LastName", 3, 30, value, false);
+				this.lastName = value;
+			}
+		}
 
-        public string ProfilePicture {
-            get => this.profilePicture;
-            set {
-                ValidateURL(value);
-                this.profilePicture = value;
-            }
-        }
+		public string ProfilePicture {
+			get => this.profilePicture;
+			set {
+				ValidateURL(value);
+				this.profilePicture = value;
+			}
+		}
+		
+		public List<User<T>> Friends { get; set; }
 
-        public List<User<T>> Friends { get; set; }
-        
-        /// <summary>
-        /// Throws an argument exception if the given value is not composed only of letters, and if specified, also of digits.
-        /// Does nothing otherwise.
-        /// </summary>
-        private static void ValidateString(string name, int minLength, int maxLength, string value, bool canBeDigit) {
-            if (value.Length < minLength || value.Length > maxLength)
-                    throw new ArgumentException($"{name} length cannot be less than {minLength} and more than {maxLength}.");
+		/// <summary>
+		/// Throws an argument exception if the given value is not composed only of letters, and if specified, also of digits.
+		/// Does nothing otherwise.
+		/// </summary>
+		private static void ValidateString(string name, int minLength, int maxLength, string value, bool canBeDigit) {
+			if (value.Length < minLength || value.Length > maxLength)
+				throw new ArgumentException($"{name} length cannot be less than {minLength} and more than {maxLength}.");
 
-            foreach (char character in value) { // more efficient than Linq
-                if (!Char.IsLetter(character) || (canBeDigit && !Char.IsDigit(character)))
-                    throw new ArgumentException($"{name} contains invalid characters."); 
-            }
-        }
+			foreach (char character in value) { // more efficient than Linq
+				if (!Char.IsLetter(character) || (canBeDigit && !Char.IsDigit(character)))
+					throw new ArgumentException($"{name} contains invalid characters."); 
+			}
+		}
 
-        /// <summary>
-        /// Throws an exception if the absolute url isn't valid.
-        /// Does nothing otherwise.
-        /// </summary>
-        private static void ValidateURL(string urlValue) {
-            // Throws an error is URL is invalid
-            Uri validatedUri;
-            Uri.TryCreate(urlValue, UriKind.Absolute, out validatedUri);
-        }
+		/// <summary>
+		/// Throws an exception if the absolute url isn't valid.
+		/// Does nothing otherwise.
+		/// </summary>
+		private static void ValidateURL(string urlValue) {
+			// Throws an error is URL is invalid
+			Uri validatedUri;
+			Uri.TryCreate(urlValue, UriKind.Absolute, out validatedUri);
+		}
 	}
 }
