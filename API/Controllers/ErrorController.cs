0bd15454f29e30438b28ca0e27887de6b765fb11
Syndamia <kamen.d.mladenov@protonmail.com>
2020-12-10 21:23:04 +0200

    Added temporary functionality to ErrorController

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
 	[ApiController]
+	[Route("/api/[controller]")]
 	public class ErrorController
 	{
 		public HttpStatusCode Error(Exception exception)
 		{
-			
+			return HttpStatusCode.OK;
 		}
 	}
 }
