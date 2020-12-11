c63b86e74230144c111a380b272a0260e6ed019b
Syndamia <kamen.d.mladenov@protonmail.com>
2020-12-11 21:32:41 +0200

    Added UserRole migrations

--- API/Migrations/20201211193121_UserRole.Designer.cs
+++ API/Migrations/20201211193121_UserRole.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using API.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace API.Migrations
 {
     [DbContext(typeof(DevHiveContext))]
-    partial class DevHiveContextModelSnapshot : ModelSnapshot
+    [Migration("20201211193121_UserRole")]
+    partial class UserRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
