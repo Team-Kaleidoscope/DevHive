9c034d3992d7e39e39375dc88dcc38a5b6abc782
Danail Dimitrov <danaildimitrov321@gmail.com>
2020-12-15 20:54:13 +0200

    Adding LanguageMappings and LanguageServiceModel

---
+++ src/DevHive.Services/Configurations/Mapping/LanguageMappings.cs
@@ -0,0 +1,14 @@
+using DevHive.Data.Models;
+using AutoMapper;
+using DevHive.Services.Models.Language;
+
+namespace DevHive.Services.Configurations.Mapping
+{
+	public class LanguageMappings : Profile
+	{
+		public LanguageMappings()
+		{
+			CreateMap<LanguageServiceModel, Language>();
+		}
+	}
+}
---
+++ src/DevHive.Services/Models/Language/LanguageServiceModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DevHive.Services.Models.Language
+{
+	public class LanguageServiceModel
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+	}
+}
