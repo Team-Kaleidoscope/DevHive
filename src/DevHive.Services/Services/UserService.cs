dc27cec6b3dd631c0f9a4e482743a053cf766df6
transtrike <transtrike@gmail.com>
2020-12-15 23:03:19 +0200

    Fixed JWT Token generation

--- src/DevHive.Services/Services/UserService.cs
+++ src/DevHive.Services/Services/UserService.cs
@@ -11,6 +11,7 @@ using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Cryptography;
 using System.Text;
+using System.Collections.Immutable;
 
 namespace DevHive.Services.Services
 {
