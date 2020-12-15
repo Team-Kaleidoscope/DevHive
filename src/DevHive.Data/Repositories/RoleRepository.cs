0f35d898c2d0bbc84196ad254dd94a26852e3e4e
Syndamia <kamen.d.mladenov@protonmail.com>
2020-12-15 20:13:24 +0200

    Implemented RoleService and updated RoleRepository structure

--- src/DevHive.Data/Repositories/RoleRepository.cs
+++ src/DevHive.Data/Repositories/RoleRepository.cs
@@ -2,30 +2,45 @@ using System;
 using System.Threading.Tasks;
 using Data.Models.Interfaces.Database;
 using DevHive.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevHive.Data.Repositories
 {
 	public class RoleRepository : IRepository<Role>
 	{
+		private readonly DbContext _context;
+
+		public RoleRepository(DbContext context)
+		{
+			this._context = context;
+		}
+
 		public async Task AddAsync(Role entity)
 		{
 			throw new NotImplementedException();
 		}
 
-		//Find entity by id
 		public async Task<Role> GetByIdAsync(Guid id)
 		{
 			throw new NotImplementedException();
 		}
 
-		//Modify Entity from database
 		public async Task EditAsync(Role newEntity)
 		{
 			throw new NotImplementedException();
 		}
 
-		//Delete Entity from database
 		public async Task DeleteAsync(Role entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<bool> DoesNameExist(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<bool> DoesRoleExist(Guid id)
 		{
 			throw new NotImplementedException();
 		}
