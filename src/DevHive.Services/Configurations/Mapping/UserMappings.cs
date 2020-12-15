7c67fa1ad52ca72964b3cf3918d394364bb5cb66
transtrike <transtrike@gmail.com>
2020-12-15 19:23:42 +0200

    Configurations folder with Mappings added

---
+++ src/DevHive.Services/Configurations/Mapping/UserMappings.cs
@@ -0,0 +1,16 @@
+using DevHive.Data.Models;
+using AutoMapper;
+using DevHive.Services.Models.Identity.User;
+
+namespace DevHive.Services.Configurations.Mapping
+{
+	public class UserMappings : Profile
+	{
+		public UserMappings()
+		{
+			CreateMap<UserServiceModel, User>();
+			CreateMap<RegisterServiceModel, User>();
+			CreateMap<UpdateUserServiceModel, User>();
+		}
+	} 
+}
