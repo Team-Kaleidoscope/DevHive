7f250088599064efb9ccb278b0fa684232f845c6
Danail Dimitrov <danaildimitrov321@gmail.com>
2020-12-16 22:56:36 +0200

    Adding TechnologyRepository and Technology model

--- src/DevHive.Data/Models/Technology.cs
+++ src/DevHive.Data/Models/Technology.cs
@@ -5,5 +5,7 @@ namespace DevHive.Data.Models
 	public class Technology : IModel
 	{
 		public Guid Id { get; set; }
+
+		public string Name { get; set; }
 	}
 }
---
+++ src/DevHive.Data/Repositories/TechnologyRepository.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading.Tasks;
+using Data.Models.Interfaces.Database;
+using DevHive.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using DevHive.Common.Models.Data;
+
+namespace DevHive.Data.Repositories
+{
+	public class TechnologyRepository : IRepository<Technology>
+	{
+		private readonly DbContext _context;
+
+		public TechnologyRepository(DbContext context)
+		{
+			this._context = context;
+		}
+
+		//Create
+		public async Task<bool> AddAsync(Technology entity)
+		{
+			await this._context
+				.Set<Technology>()
+				.AddAsync(entity);
+
+			return await RepositoryMethods.SaveChangesAsync(this._context);
+		}
+
+		//Read		
+		public async Task<Technology> GetByIdAsync(Guid id)
+		{
+			return await this._context
+				.Set<Technology>()
+				.FindAsync(id);
+		}
+
+		public async Task<bool> DoesTechnologyNameExist(string technologyName)
+		{
+			return await this._context
+				.Set<Technology>()
+				.AsNoTracking()
+				.AnyAsync(r => r.Name == technologyName);
+		}
+
+		public async Task<bool> DoesTechnologyExist(Guid id)
+		{
+			return await this._context
+				.Set<Technology>()
+				.AsNoTracking()
+				.AnyAsync(r => r.Id == id);
+		}
+
+		//Edit
+		public async Task<bool> EditAsync(Technology newEntity)
+		{
+				this._context
+				.Set<Technology>()
+				.Update(newEntity);
+
+			return await RepositoryMethods.SaveChangesAsync(this._context);
+		}
+
+		//Delete
+		public async Task<bool> DeleteAsync(Technology entity)
+		{
+			this._context
+				.Set<Technology>()
+				.Remove(entity);
+
+			return await RepositoryMethods.SaveChangesAsync(this._context);
+		}
+	}
+}
