5bc055720bd1218af76bafb7437d5150424f6191
Syndamia <kamen.d.mladenov@protonmail.com>
2020-12-16 12:31:49 +0200

    Removed unecessary Data annotations in data user model

--- src/DevHive.Data/Models/User.cs
+++ src/DevHive.Data/Models/User.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
@@ -8,24 +7,14 @@ namespace DevHive.Data.Models
 	[Table("Users")]
 	public class User : IdentityUser<Guid>, IModel
 	{
-		[Required]
-		[MinLength(3)]
-		[MaxLength(50)]
-		[Display(Name = "Username")]
 		public override string UserName
 		{
 			get => base.UserName;
 			set => base.UserName = value;
 		}
 
-		[Required]
-		[MinLength(3)]
-		[MaxLength(30)]
 		public string FirstName { get; set; }
 
-		[Required]
-		[MinLength(3)]
-		[MaxLength(30)]
 		public string LastName { get; set; }
 
 		public string ProfilePicture { get; set; }
