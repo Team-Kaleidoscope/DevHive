b24b9b1ffa528f29aa87f0e48097a35386fbb8b1
transtrike <transtrike@gmail.com>
2020-12-17 20:15:31 +0200

    Fixed roles not mapping properly

--- src/DevHive.Services/Models/Identity/User/UserServiceModel.cs
+++ src/DevHive.Services/Models/Identity/User/UserServiceModel.cs
@@ -5,7 +5,7 @@ namespace DevHive.Services.Models.Identity.User
 {
 	public class UserServiceModel : BaseUserServiceModel
 	{
-		public IList<RoleModel> Role { get; set; } = new List<RoleModel>();
+		public IList<RoleModel> Roles { get; set; } = new List<RoleModel>();
 		public List<UserServiceModel> Friends { get; set; } = new List<UserServiceModel>();
 	}
 }
