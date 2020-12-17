f4515fc3ff5fc222a3bdd40c5d4113f9bd79106f
transtrike <transtrike@gmail.com>
2020-12-17 19:12:17 +0200

    Newtonsoft.Json added

--- src/DevHive.Web/Startup.cs
+++ src/DevHive.Web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using DevHive.Web.Configurations.Extensions;
 using AutoMapper;
+using Newtonsoft.Json;
 
 namespace DevHive.Web
 {
@@ -21,7 +22,11 @@ namespace DevHive.Web
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllers();
+			services.AddControllers()
+				.AddNewtonsoftJson(x => 
+				{
+					x.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+				});
 
 			services.DatabaseConfiguration(Configuration);
 			services.SwaggerConfiguration();
