e2958817c79f29722fedc1ea8ddcedea894a0f20
transtrike <transtrike@gmail.com>
2020-12-21 22:15:18 +0200

    Exception handling added

--- src/DevHive.Web/Controllers/ErrorController.cs
+++ src/DevHive.Web/Controllers/ErrorController.cs
@@ -1,17 +1,46 @@
 using System;
-using System.Net.Http;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DevHive.Web.Controllers
 {
-	[ApiController]
-	[Route("/api/[controller]")]
-	public class ErrorController
+	public class ErrorController : ControllerBase
 	{
-		[HttpGet]
-		public IActionResult Error(Exception exception)
+		[HttpPost]
+		[Route("/api/Error")]
+		public IActionResult Error()
 		{
-			return new BadRequestObjectResult(exception);
+			//Later for logging
+			string requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+			IExceptionHandlerFeature exception =
+			HttpContext.Features.Get<IExceptionHandlerFeature>();
+		
+			object result = ProcessException(requestId, exception);
+			return new BadRequestObjectResult(JsonConvert.SerializeObject(result));
+		}
+
+		private object ProcessException(string requestId, IExceptionHandlerFeature exception)
+		{
+			switch (exception.Error)
+			{
+				case ArgumentException _:
+				case InvalidOperationException _:
+					return MessageToObject(exception.Error.Message);
+				default:
+					return MessageToObject(null);
+			}
+		}
+
+		private object MessageToObject(string message)
+		{
+			return new
+			{
+				Error = message
+			};
 		}
 	}
 }
