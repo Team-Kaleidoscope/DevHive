6770bf97d05aa116fda03ed45a828505ee9ba235
Syndamia <kamen.d.mladenov@protonmail.com>
2021-01-08 14:08:39 +0200

    Implemented latest login dev UI, add functionality (result from API is shown in console); updated tsconfig

--- src/DevHive.Angular/src/app/app.module.ts
+++ src/DevHive.Angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/DevHive.Angular/src/app/login/login.component.ts
+++ src/DevHive.Angular/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  private _title = 'Login';
 
-  ngOnInit(): void {
+  constructor(private titleService: Title, private fb: FormBuilder) {
+    titleService.setTitle(this._title);
   }
 
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      userName: '',
+      password: ''
+    });
+  }
+
+  async onSubmit(): Promise<void> {
+    const response = await fetch('http://localhost:5000/api/User/login', {
+      method: 'POST',
+      body: `{"UserName": "${this.loginForm.get('userName')?.value}", "Password": "${this.loginForm.get('password')?.value}"}`,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log(response);
+  }
 }
