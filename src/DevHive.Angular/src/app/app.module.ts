16545b6637743d97f18e9aa45f6eeaec81038269
Syndamia <kamen.d.mladenov@protonmail.com>
2021-01-08 14:48:05 +0200

    Moved login page to app/components; fixed, updated and improved styling of register and login pages

--- src/DevHive.Angular/src/app/app.module.ts
+++ src/DevHive.Angular/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
