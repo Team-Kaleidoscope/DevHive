c7fc731a94939c2f5ea5fa7a123dcf06009136d4
Syndamia <kamen.d.mladenov@protonmail.com>
2021-01-14 10:01:12 +0200

    Added simple routing, where on / is the feed, on /login is login and on /register is register

--- src/DevHive.Angular/src/app/app-routing.module.ts
+++ src/DevHive.Angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedComponent } from './components/feed/feed.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FeedComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
