18e7ba3a2ee9460905507ba1f1605bc7a4fdd150
Syndamia <kamen.d.mladenov@protonmail.com>
2021-01-29 17:35:54 +0200

    Implemented angular languages and technologies service requests for getting data

--- src/DevHive.Angular/src/app/services/technology.service.ts
+++ src/DevHive.Angular/src/app/services/technology.service.ts
@@ -2,6 +2,7 @@ import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Guid} from 'guid-typescript';
 import {Observable} from 'rxjs';
+import {Technology} from 'src/models/identity/user';
 import {AppConstants} from '../app-constants.module';
 
 @Injectable({
@@ -16,4 +17,44 @@ export class TechnologyService {
     };
     return this.http.get(AppConstants.API_TECHNOLOGY_URL, options);
   }
+
+  getAllTechnologiesWithSessionStorageRequest(): Observable<object> {
+    const token = sessionStorage.getItem('UserCred') ?? '';
+    return this.getAllTechnologiesRequest(token);
+  }
+
+  getAllTechnologiesRequest(authToken: string): Observable<object> {
+    const options = {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + authToken)
+    };
+    return this.http.get(AppConstants.API_TECHNOLOGY_URL + '/GetTechnologies', options);
+  }
+
+  getFullTechnologiesFromIncomplete(givenTechnologies: Technology[]): Promise<Technology[]> {
+    if (givenTechnologies.length === 0) {
+      return new Promise(resolve => resolve(givenTechnologies));
+    }
+
+    // This accepts language array with incomplete languages, meaning
+    // languages that only have an id, but no name
+    return new Promise(resolve => {
+      const lastGuid = givenTechnologies[givenTechnologies.length - 1].id;
+
+      // For each language, request his name and assign it
+      for (const tech of givenTechnologies) {
+        this.getTechnologyRequest(tech.id).subscribe(
+          (result: object) => {
+            // this only assigns the "name" property to the language,
+            // because only the name is returned from the request
+            Object.assign(tech, result);
+
+            if (lastGuid === tech.id) {
+              resolve(givenTechnologies);
+            }
+          }
+        );
+      }
+    });
+  }
+
 }
