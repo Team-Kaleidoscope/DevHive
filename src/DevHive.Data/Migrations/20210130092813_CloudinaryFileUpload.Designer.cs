8b164bf4ac8c307de933b476e858e489a64c0da5
Syndamia <kamen.d.mladenov@protonmail.com>
2021-01-30 11:31:48 +0200

    Added cloud implementation migration

--- src/DevHive.Data/Migrations/20210130092813_CloudinaryFileUpload.Designer.cs
+++ src/DevHive.Data/Migrations/20210130092813_CloudinaryFileUpload.Designer.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using DevHive.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DevHive.Data.Migrations
 {
     [DbContext(typeof(DevHiveContext))]
-    partial class DevHiveContextModelSnapshot : ModelSnapshot
+    [Migration("20210130092813_CloudinaryFileUpload")]
+    partial class CloudinaryFileUpload
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
