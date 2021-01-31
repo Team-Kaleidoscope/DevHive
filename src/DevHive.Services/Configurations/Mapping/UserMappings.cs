505bc41720cbcd02d65e17a6440931c87abcdeda
transtrike <transtrike@gmail.com>
2021-01-31 13:34:49 +0200

    Fixed mapping

--- src/DevHive.Services/Configurations/Mapping/UserMappings.cs
+++ src/DevHive.Services/Configurations/Mapping/UserMappings.cs
@@ -2,6 +2,7 @@ using DevHive.Data.Models;
 using AutoMapper;
 using DevHive.Services.Models.Identity.User;
 using DevHive.Common.Models.Misc;
+using DevHive.Data.RelationModels;
 
 namespace DevHive.Services.Configurations.Mapping
 {
@@ -11,12 +12,17 @@ namespace DevHive.Services.Configurations.Mapping
 		{
 			CreateMap<UserServiceModel, User>();
 			CreateMap<RegisterServiceModel, User>();
-			CreateMap<FriendServiceModel, User>();
+			CreateMap<FriendServiceModel, User>()
+				.ForMember(dest => dest.Friends, src => src.Ignore());
+			CreateMap<UserFriends, FriendServiceModel>()
+				.ForMember(dest => dest.UserName, src => src.MapFrom(p => p.Friend.UserName));
 			CreateMap<UpdateUserServiceModel, User>()
+				.ForMember(dest => dest.Friends, src => src.Ignore())
 				.AfterMap((src, dest) => dest.PasswordHash = PasswordModifications.GeneratePasswordHash(src.Password));
 			CreateMap<UpdateFriendServiceModel, User>();
 
-			CreateMap<User, UserServiceModel>();
+			CreateMap<User, UserServiceModel>()
+				.ForMember(dest => dest.Friends, src => src.MapFrom(p => p.Friends));
 			CreateMap<User, UpdateUserServiceModel>()
 				.ForMember(x => x.Password, opt => opt.Ignore());
 			CreateMap<User, FriendServiceModel>();
