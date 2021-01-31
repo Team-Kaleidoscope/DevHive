0ba05c05afce1ef99a685ecc7f4ef8a24fa50dd3
Syndamia <kamen.d.mladenov@protonmail.com>
2021-01-31 11:54:34 +0200

    Added a "Back to feed" button to post page

--- src/DevHive.Angular/src/app/components/post-page/post-page.component.ts
+++ src/DevHive.Angular/src/app/components/post-page/post-page.component.ts
@@ -17,4 +17,7 @@ export class PostPageComponent implements OnInit {
     this.postId = Guid.parse(this._router.url.substring(6));
   }
 
+  backToFeed(): void {
+    this._router.navigate(['/']);
+  }
 }
