7f104af0a8e4004724a985499d155a217b7735e9
Syndamia <kamen.d.mladenov@protonmail.com>
2021-02-05 19:14:57 +0200

    Fixed getting feed

--- src/DevHive.Services/Services/FeedService.cs
+++ src/DevHive.Services/Services/FeedService.cs
@@ -42,13 +42,11 @@ namespace DevHive.Services.Services
 			if (user == null)
 				throw new ArgumentException("User doesn't exist!");
 
-			// List<User> friendsList = user.Friends.Select(x => x.Friend).ToList();
-			// if (friendsList.Count == 0)
-			// 	throw new ArgumentException("User has no friends to get feed from!");
+			if (user.Friends.Count == 0)
+				throw new ArgumentException("User has no friends to get feed from!");
 
-			List<Post> posts = new();
-			// await this._feedRepository
-			// 	.GetFriendsPosts(friendsList, model.FirstRequestIssued, model.PageNumber, model.PageSize);
+			List<Post> posts = await this._feedRepository
+				.GetFriendsPosts(user.Friends.ToList(), model.FirstRequestIssued, model.PageNumber, model.PageSize);
 
 			ReadPageServiceModel readPageServiceModel = new();
 			foreach (Post post in posts)
