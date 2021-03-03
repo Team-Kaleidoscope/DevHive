b9a9d63418d728560b17c23d49255328d16ccb11
transtrike <transtrike@gmail.com>
2021-03-03 18:35:12 +0200

    NSwag replaced Swagger; Doc prep configured

--- src/Web/DevHive.Web/Startup.cs
+++ src/Web/DevHive.Web/Startup.cs
@@ -48,7 +48,6 @@ namespace DevHive.Web
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
-				app.UseSwaggerConfiguration();
 			}
 			else
 			{
@@ -56,6 +55,7 @@ namespace DevHive.Web
 				app.UseExceptionHandlerMiddlewareConfiguration();
 			}
 
+			app.UseSwaggerConfiguration();
 			app.UseDatabaseConfiguration();
 			app.UseAutoMapperConfiguration();
 
