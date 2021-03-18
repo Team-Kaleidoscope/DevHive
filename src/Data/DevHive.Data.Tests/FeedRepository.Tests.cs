3b1200b7622cff65d56ae63cdfc074b19a822e75
Danail Dimitrov <danaildimitrov321@gmail.com>
2021-03-18 21:32:29 +0200

    Adding test for untested methods in FeedRepository

--- src/Data/DevHive.Data.Tests/FeedRepository.Tests.cs
+++ src/Data/DevHive.Data.Tests/FeedRepository.Tests.cs
@@ -11,6 +11,9 @@ namespace DevHive.Data.Tests
 	[TestFixture]
 	public class FeedRepositoryTests
 	{
+		private const int PAGE_NUMBER = 1;
+		private const int PAGE_SIZE = 10;
+
 		private DevHiveContext _context;
 		private FeedRepository _feedRepository;
 
@@ -37,45 +40,66 @@ namespace DevHive.Data.Tests
 		[Test]
 		public async Task GetFriendsPosts_ReturnsListOfPosts_WhenTheyExist()
 		{
-			User dummyUser = CreateDummyUser();
+			User dummyUser = this.CreateDummyUser();
+			dummyUser.Posts = await this.CreateDummyPosts(dummyUser);
 			List<User> friendsList = new()
 			{
 				dummyUser
 			};
 
-			DateTime dateTime = new(3000, 05, 09, 9, 15, 0);
-			Console.WriteLine(dateTime.ToFileTime());
-
-			const int PAGE_NUMBER = 1;
-			const int PAGE_SIZE = 10;
+			DateTime dateTime = DateTime.Now;
 
 			List<Post> resultList = await this._feedRepository.GetFriendsPosts(friendsList, dateTime, PAGE_NUMBER, PAGE_SIZE);
 
-			Assert.GreaterOrEqual(2, resultList.Count, "GetFriendsPosts does not return all correct posts");
+			Assert.GreaterOrEqual(resultList.Count, dummyUser.Posts.Count, "GetFriendsPosts does not return the posts corrtrectly");
 		}
 
 		[Test]
-		public async Task GetFriendsPosts_ReturnsNull_WhenNoSuitablePostsExist()
+		public async Task GetFriendsPosts_ReturnsEmptyList_WhenNoSuitablePostsExist()
 		{
-			User dummyUser = CreateDummyUser();
+			User dummyUser = this.CreateDummyUser();
 			List<User> friendsList = new()
 			{
 				dummyUser
 			};
 
-			DateTime dateTime = new(3000, 05, 09, 9, 15, 0);
-
-			const int PAGE_NUMBER = 1;
-			const int PAGE_SIZE = 10;
+			DateTime dateTime = DateTime.Now;
 
 			List<Post> resultList = await this._feedRepository.GetFriendsPosts(friendsList, dateTime, PAGE_NUMBER, PAGE_SIZE);
 
-			Assert.LessOrEqual(0, resultList.Count, "GetFriendsPosts does not return all correct posts");
+			Assert.LessOrEqual(resultList.Count, 0, "GetFriendsPosts does not return all correct posts");
+		}
+		#endregion
+
+		#region GetUsersPosts
+		[Test]
+		public async Task GetUsersPosts_ReturnsAllPostsOfTheUser_IfAnyExist()
+		{
+			User dummyUser = this.CreateDummyUser();
+			HashSet<Post> posts = await this.CreateDummyPosts(dummyUser);
+
+			DateTime dateTime = DateTime.Now;
+
+			List<Post> resultList = await this._feedRepository.GetUsersPosts(dummyUser, dateTime, PAGE_NUMBER, PAGE_SIZE);
+
+			Assert.GreaterOrEqual(resultList.Count, posts.Count, "GetUsersPosts does not return the posts corrtrectly");
+		}
+
+		[Test]
+		public async Task GetUsersPosts_ReturnsEmptyList_WhenNoSuitablePostsExist()
+		{
+			User dummyUser = this.CreateDummyUser();
+
+			DateTime dateTime = DateTime.Now;
+
+			List<Post> resultList = await this._feedRepository.GetUsersPosts(dummyUser, dateTime, PAGE_NUMBER, PAGE_SIZE);
+
+			Assert.LessOrEqual(resultList.Count, 0, "GetUsersPosts does not return empty list when no suitable posts exist");
 		}
 		#endregion
 
 		#region HelperMethods
-		private static User CreateDummyUser()
+		private User CreateDummyUser()
 		{
 			HashSet<Role> roles = new()
 			{
@@ -95,6 +119,21 @@ namespace DevHive.Data.Tests
 				Email = "abv@abv.bg",
 				Roles = roles
 			};
+		}
+
+		private async Task<HashSet<Post>> CreateDummyPosts(User user)
+		{
+			HashSet<Post> posts = new HashSet<Post>
+			{
+				new Post { Creator = user, TimeCreated = DateTime.Now },
+				new Post{ Creator = user, TimeCreated = DateTime.Now },
+				new Post{ Creator = user, TimeCreated = DateTime.Now }
+			};
+
+			await this._context.Posts.AddRangeAsync(posts);
+			await this._context.SaveChangesAsync();
+
+			return posts;
 		}
 		#endregion
 	}
