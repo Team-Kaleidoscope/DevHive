a58b73c11e9d017fcae59f2820a68b88888267ba
transtrike <transtrike@gmail.com>
2021-03-26 17:25:07 +0200

    Splitted validation in Service

--- src/Services/DevHive.Services/Services/ProfilePictureService.cs
+++ src/Services/DevHive.Services/Services/ProfilePictureService.cs
@@ -24,7 +24,8 @@ namespace DevHive.Services.Services
 
 		public async Task<string> InsertProfilePicture(ProfilePictureServiceModel profilePictureServiceModel)
 		{
-			await ValidateServiceModel(profilePictureServiceModel);
+			ValidateProfPic(profilePictureServiceModel.ProfilePictureFormFile);
+			await ValidateUserExistsAsync(profilePictureServiceModel.UserId);
 
 			return await SaveProfilePictureInDatabase(profilePictureServiceModel);
 		}
@@ -36,7 +37,8 @@ namespace DevHive.Services.Services
 
 		public async Task<string> UpdateProfilePicture(ProfilePictureServiceModel profilePictureServiceModel)
 		{
-			await ValidateServiceModel(profilePictureServiceModel);
+			ValidateProfPic(profilePictureServiceModel.ProfilePictureFormFile);
+			await ValidateUserExistsAsync(profilePictureServiceModel.UserId);
 
 			User user = await this._userRepository.GetByIdAsync(profilePictureServiceModel.UserId);
 			if (!string.IsNullOrEmpty(user.ProfilePicture.PictureURL))
@@ -87,12 +89,15 @@ namespace DevHive.Services.Services
 			return picUrl;
 		}
 
-		private async Task ValidateServiceModel(ProfilePictureServiceModel profilePictureServiceModel)
+		private static void ValidateProfPic(IFormFile profilePictureFormFile)
 		{
-			if (profilePictureServiceModel.ProfilePictureFormFile.Length == 0)
+			if (profilePictureFormFile.Length == 0)
 				throw new ArgumentException("Picture cannot be null");
+		}
 
-			if (!await this._userRepository.DoesUserExistAsync(profilePictureServiceModel.UserId))
+		private async Task ValidateUserExistsAsync(Guid userId)
+		{
+			if (!await this._userRepository.DoesUserExistAsync(userId))
 				throw new ArgumentException("User does not exist!");
 		}
 	}
