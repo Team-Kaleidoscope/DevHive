418c04807eae6220ef609a8c208d67c08cf7f723
transtrike <transtrike@gmail.com>
2021-03-28 21:58:50 +0300

    Added profile picture documentation

--- src/Services/DevHive.Services/Interfaces/IProfilePictureService.cs
+++ src/Services/DevHive.Services/Interfaces/IProfilePictureService.cs
@@ -6,17 +6,32 @@ namespace DevHive.Services.Interfaces
 {
 	public interface IProfilePictureService
 	{
+		/// <summary>
+		/// Inserts a user's profile picture in the database and cloud
+		/// </summary>
+		/// <param name="profilePictureServiceModel">User's Guid and his/hers profile picture as file</param>
+		/// <returns>The new profile picture's URL in the cloud</returns>
 		Task<string> InsertProfilePicture(ProfilePictureServiceModel profilePictureServiceModel);
 
+		/// <summary>
+		/// Get a profile picture by it's Guid
+		/// </summary>
+		/// <param name="id">Profile picture's Guid</param>
+		/// <returns>The profile picture's URL in the cloud</returns>
 		Task<string> GetProfilePictureById(Guid id);
 
 		/// <summary>
 		/// Uploads the given picture and assigns it's link to the user in the database
 		/// </summary>
 		/// <param name="profilePictureServiceModel">Contains User's Guid and the new picture to be updated</param>
-		/// <returns>The new picture's URL</returns>
+		/// <returns>The new profile picture's URL in the cloud</returns>
 		Task<string> UpdateProfilePicture(ProfilePictureServiceModel profilePictureServiceModel);
 
+		/// <summary>
+		/// Delete a profile picture from the cloud and the database
+		/// </summary>
+		/// <param name="id">The profile picture's Guid</param>
+		/// <returns>True if the picture is deleted, false otherwise</returns>
 		Task<bool> DeleteProfilePicture(Guid id);
 	}
 }
