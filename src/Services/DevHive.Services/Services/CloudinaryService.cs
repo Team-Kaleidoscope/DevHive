0a58b0c36f113b41984629aff45a9162305300a4
Syndamia <kamen.d.mladenov@protonmail.com>
2021-04-07 21:11:15 +0300

    Fixed remove files from cloud workaround throwin error when called

--- src/Services/DevHive.Services/Services/CloudinaryService.cs
+++ src/Services/DevHive.Services/Services/CloudinaryService.cs
@@ -51,7 +51,7 @@ namespace DevHive.Services.Services
 		public async Task<bool> RemoveFilesFromCloud(List<string> fileUrls)
 		{
 			// Workaround, this method isn't fully implemented yet
-			await Task.Run(null);
+			await Task.Run(() => {});
 
 			return true;
 		}
